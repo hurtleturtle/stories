58f4f420f947397f06978e835fdb4fc7b06ea24f
Jono <jono.nicholas@hotmail.co.uk>
2020-09-04 21:08:07 +0100

    added self.base_url + updated load_webpage for 404

--- story.py
+++ story.py
@@ -17,8 +17,13 @@ import traceback
 class Story():
     def __init__(self, args):
         self.initial_url = args['url']
+
+        # get base url of site
+        parsed_url = urlparse(self.initial_url)
+        self.base_url = parsed_url.scheme + '://' + parsed_url.hostname + '/'
         self.debug = args.get('verbosity', 0)
         self.container = args.get('container', 'div.chapter-content')
+        self.next = args.get('next', 'a#next_chap')
         self.story = self.init_story()
 
     def init_story(self, template_file='template.html'):
@@ -35,6 +40,8 @@ class Story():
 
         if response.status_code == 200:
             return response.text
+        elif response.status_code == 404:
+            return '404'
         else:
             return False
 
@@ -55,13 +62,25 @@ class Story():
             for tag in filtered:
                 self.story.body.append(tag)
 
+    def get_next_url(self, soup):
+        next_url = soup.select(self.next)
+
+        if next_url:
+            return self.base_url + next_url[0]['href']
+        else:
+            print('Could not get next url.')
+            return ''
+
 
 if __name__ == '__main__':
     s = Story({'url': 'https://novelfull.com/' +
                'god-of-slaughter/chapter-119-the-devil-king-bo-xun.html',
                'verbosity': 1,
-               'container': 'div.chapter-c > p'})
+               'container': 'div.chapter-c > p',
+               'next': 'a#next_chap'})
     page = s.load_webpage(s.initial_url)
     soup = s.load_soup(page)
-    s.filter_story_content(soup)
+    s.process_story_content(soup)
+    n = s.get_next_url(soup)
     print(s.story.prettify())
+    print(n)
