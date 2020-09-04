f1c4784df7e31bbf2c98d5d6a80f37e7cfcc1a53
Jono <jono.nicholas@hotmail.co.uk>
2020-09-04 20:51:08 +0100

    process_story_content written

--- story.py
+++ story.py
@@ -15,9 +15,10 @@ import traceback
 
 
 class Story():
-    def __init__(self, args, verbosity=0):
+    def __init__(self, args):
         self.initial_url = args['url']
-        self.debug = verbosity
+        self.debug = args.get('verbosity', 0)
+        self.container = args.get('container', 'div.chapter-content')
         self.story = self.init_story()
 
     def init_story(self, template_file='template.html'):
@@ -39,3 +40,28 @@ class Story():
 
     def load_soup(self, page):
         return BeautifulSoup(page, features='lxml')
+
+    def process_story_content(self, soup, container=None):
+        if not container:
+            container = self.container
+
+        # filter chapter content
+        filtered = soup.select(container)
+
+        # add chapter content to story
+        if len(filtered) < 1:
+            print('Container not found.')
+        else:
+            for tag in filtered:
+                self.story.body.append(tag)
+
+
+if __name__ == '__main__':
+    s = Story({'url': 'https://novelfull.com/' +
+               'god-of-slaughter/chapter-119-the-devil-king-bo-xun.html',
+               'verbosity': 1,
+               'container': 'div.chapter-c > p'})
+    page = s.load_webpage(s.initial_url)
+    soup = s.load_soup(page)
+    s.filter_story_content(soup)
+    print(s.story.prettify())
