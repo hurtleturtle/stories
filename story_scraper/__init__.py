61959299ffda9a1b34e8a9b94b25acccaa4b7fb3
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-09-30 18:54:26 +0100

    Added blueprint and nav.html

--- story_scraper/__init__.py
+++ story_scraper/__init__.py
@@ -4,4 +4,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, instance_relative_config=True, template_folder='routes/templates')
 
+
+    from story_scraper.routes import base
+    app.register_blueprint(base.bp)
+
     return app
--- story_scraper/routes/base.py
+++ story_scraper/routes/base.py
@@ -4,6 +4,6 @@ from flask import Blueprint, render_template
 bp = Blueprint('base', __name__, template_folder='templates/base')
 
 
-@bp.route('')
+@bp.route('', strict_slashes=False)
 def index():
     return render_template('base.html')
